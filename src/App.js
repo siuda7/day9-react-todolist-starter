9b886867f05c70196f51842c164d3f4db332a58d
Daniel Siu <daniel.siu@oocl.com>
2024-11-29 10:42:37 +0800

    feat: add Router and NotFoundPage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { createContext, useReducer } from "react";
 import './App.css';
 import TodoList from "./components/TodoList";
 import { initialState, todoReducer } from "./context/todoReducer";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import NotFoundPage from "./components/NotFoundPage";
 
 export const TodoContext = createContext();
 
@@ -11,7 +13,13 @@ function App() {
   return (
     <div className="App">
       <TodoContext.Provider value={{ state, dispatch }}>
-        <TodoList/>
+        <Router>
+          <Routes>
+            <Route path={"/todo-list"} element={<TodoList />}></Route>
+            <Route path={"/"} element={<Navigate to="/todo-list"/>}></Route>
+            <Route path={"*"} element={<NotFoundPage />}></Route>
+          </Routes>
+        </Router>
       </TodoContext.Provider>
     </div>
   );
---
+++ src/components/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+    return (
+        <>
+        <img src='/404_01-min.jpg' />
+        </>
+    )
+}
+
+export default NotFoundPage
